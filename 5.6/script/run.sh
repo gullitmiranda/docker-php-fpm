29b9e51327f979070b9eeb6790d32ce410286e24
Gullit Miranda <gullitmiranda@gmail.com>
2015-10-13 18:49:39 -0300

    Fixing 404 in circleci test

--- 5.6/script/run.sh
+++ 5.6/script/run.sh
@@ -10,15 +10,15 @@ elif [ ! -z "$APP_DIR" ]; then
   rm -rf "${DOCUMENT_ROOT}"
 
   if [ -d "${APP_DIR}/public" ]; then
-      dir="${DOCUMENT_ROOT}"
+    dir="${DOCUMENT_ROOT}"
   else
-      dir="${DOCUMENT_ROOT}/public"
+    dir="${DOCUMENT_ROOT}/public"
   fi
-
-  mkdir -p "dirname ${dir}"
 fi
 
 if [ ! -z "$APP_DIR" ]; then
   echo "linking public folder: ${APP_DIR} -> ${dir}"
+
+  mkdir -p "$(dirname ${dir})"
   ln -s $APP_DIR $dir
 fi

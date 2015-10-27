ad58aa19d1070cb19dd9520b82143eeb0dc9702a
Gullit Miranda <gullitmiranda@gmail.com>
2015-10-27 00:46:53 -0200

    Fixing error 505 in `5.6-debian` image

--- 5.6-debian/script/prepare.sh
+++ 5.6-debian/script/prepare.sh
@@ -9,6 +9,7 @@ sed -i \
   -e 's/user =.*/user = root/' \
   -e 's/listen\.owner.*/listen\.owner = root/' \
   -e 's/listen\.group.*/listen\.group = root/' \
+  -e 's/;listen\.allowed_clients.*/listen\.allowed_clients = 0.0.0.0/' \
   -e 's/error_log =.*/error_log = \/dev\/stdout/' \
   -e 's/;clear_env = no/clear_env = no/g' \
   /etc/php5/fpm/pool.d/www.conf
